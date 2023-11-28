f09fc58bc81d0c01f9ed0a39cda9fdc6c855aa37
itnia <itnia.aurey@outlook.com>
2023-11-29 01:54:45 +0300

    Интеграция api

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -7,79 +7,117 @@ use Illuminate\Http\Request;
 
 class UrlController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        // только для админки
+        $urls = Url::all();
+        
+        return response()->json($urls);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        // только для админки
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        // Валидация url
+        if (!filter_var($request->long_url, FILTER_VALIDATE_URL)) {
+            return response()->json([
+                'status' => 'error',
+            ]);
+        }
+        if (!empty($request->short_url) AND !filter_var($request->short_url, FILTER_VALIDATE_URL)) {
+            return response()->json([
+                'status' => 'error',
+            ]);
+        }
+
+        // ++ валидация short_url - на лишние слэши
+        // ++ валидация short_url - на длину
+
+        // Проверка на существования long_url
+        if ($res = Url::where('long_url', $request->long_url)->first()) {
+            return response()->json([
+                'status' => 'exists_long_url',
+                'long_url' => $res->long_url,
+                'short_url' => $res->short_url,
+                'number_views' => $res->number_views,
+            ]);
+        }
+
+        // Проверка на существования short_url
+        if ($res = Url::where('short_url', $request->short_url)->first()) {
+            return response()->json([
+                'status' => 'exists_short_url',
+                'long_url' => $res->long_url,
+                'short_url' => $res->short_url,
+                'number_views' => $res->number_views,
+            ]);
+        }
+
+        // Создание записи
+        $short_url = $request->short_url;
+
+        if (empty($short_url)) {
+            // сгенерировать url
+            $str = '';
+            $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+
+            do {                
+                $str = substr(str_shuffle($permitted_chars), 0, 10);
+                $short_url = config('app.url') . '/' . $str;
+            } while (Url::where('short_url', $short_url)->first()); // проверка
+        }
+
+        $url = new Url;
+        $url->long_url = $request->long_url;
+        $url->short_url = $short_url;
+        $url->number_views = 0;
+        $url->save();
+
+        return response()->json([
+            'status' => 'created',
+            'long_url' =>$url->long_url,
+            'short_url' => $url->short_url,
+            'number_views' => $url->number_views,
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Url  $url
-     * @return \Illuminate\Http\Response
-     */
     public function show(Url $url)
     {
-        //
+        $url = Url::find(1); // ?
+        return response()->json([
+            'status' => 'show',
+            'long_url' =>$url->long_url,
+            'short_url' => $url->short_url,
+            'number_views' => $url->number_views,
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Url  $url
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Url $url)
     {
-        //
+        // только для админки
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Url  $url
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Url $url)
     {
-        //
+        $url = Url::find(1); // ?
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Url  $url
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Url $url)
     {
-        //
+        $url = Url::find(1); // ?
+        $url->delete();
+    }
+
+    public function redirect($short_url)
+    {
+        if (Url::where('short_url', $short_url)->first()) {
+            redirect()->to($short_url);
+        }
+
+        abort(404);
     }
 }
